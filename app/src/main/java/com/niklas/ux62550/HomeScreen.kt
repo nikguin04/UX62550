e3cf9cdd951ae54fc39ae19c9fe4828071018e4a
Niklas Jensen <niklaspj2004@gmail.com>
2024-11-04 09:59:55 +0100

    Cleaned and pixed preview of home screen

--- app/src/main/java/com/niklas/ux62550/HomeScreen.kt
+++ app/src/main/java/com/niklas/ux62550/HomeScreen.kt
@@ -1,38 +1,28 @@
 package com.niklas.ux62550
 
-import HorizontalLazyRowWithSnapEffect
-import MediaItemsViewModel
-import android.os.Bundle
-import android.provider.MediaStore.Audio.Media
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import com.niklas.ux62550.features.MediaItemList.MediaItemsUIState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.niklas.ux62550.models.MediaItem
+import com.niklas.ux62550.modules.HorizontalLazyRowWithSnapEffect
 import com.niklas.ux62550.ui.theme.UX62550Theme
-import com.niklas.ux62550.ScreenWithGeneralNavBar
-import dk.shape.dtu.androidarchitecture.feature.fruitlist.MediaItemsList
 
 /*class HomeScreen: ComponentActivity() {
-    private val mediaItemsViewModel: MediaItemsViewModel by viewModels()
+    private val mediaItemsViewModel: com.niklas.ux62550.features.MediaItemList.MediaItemsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,28 +49,33 @@ import dk.shape.dtu.androidarchitecture.feature.fruitlist.MediaItemsList
     }
 }*/
 
-/*@Composable
+@Composable
 @Preview(showBackground = true, name = "MainPreview")
 fun HomePreview() {
+    val mediaItems = listOf(
+        MediaItem("Name 1", R.drawable.logo, Color.Blue),
+        MediaItem("Name 2", R.drawable.logo, Color.Red)
+    )
+
     UX62550Theme (darkTheme = true, dynamicColor = false) {
         ScreenWithGeneralNavBar {
-            ScreenHome()
+            ScreenHome(mediaItemsUIState = MediaItemsUIState.Data(mediaItems))
         }
     }
-}*/
+}
 
 
 @Composable
-fun ScreenHome(modifier: Modifier = Modifier, mediaItemsViewModel: MediaItemsViewModel) {
+fun ScreenHome(modifier: Modifier = Modifier, mediaItemsUIState: MediaItemsUIState) {
     Column(modifier.padding()) {
         Row(
-            modifier.fillMaxWidth().padding(FigmaPxToDp_w(29.5f), FigmaPxToDp_h(40f), 0.dp, FigmaPxToDp_h(35f)), // ConvertPxToDp(29.5f)
+            modifier.fillMaxWidth().padding(figmaPxToDp_w(29.5f), figmaPxToDp_h(40f), 0.dp, figmaPxToDp_h(35f)), // ConvertPxToDp(29.5f)
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            LogoBox(size= FigmaPxToDp_w(50f))
+            LogoBox(size= figmaPxToDp_w(50f))
 
-            Box (modifier = Modifier.padding(FigmaPxToDp_w(20f), 0.dp, 0.dp ,0.dp)) {
+            Box (modifier = Modifier.padding(figmaPxToDp_w(20f), 0.dp, 0.dp ,0.dp)) {
 
                 Text(
                     text = "Welcome, User1",
@@ -93,9 +88,10 @@ fun ScreenHome(modifier: Modifier = Modifier, mediaItemsViewModel: MediaItemsVie
             }
         }
 
+
         // Implement viewmodel
-        val uiState = mediaItemsViewModel.mediaItemsState.collectAsState().value
-        when (uiState) {
+        //val uiState = mediaItemsViewModel.mediaItemsState.collectAsState().value
+        when (mediaItemsUIState) {
             MediaItemsUIState.Empty -> {
                 Text(
                     text = "No MediasItems",
@@ -104,7 +100,7 @@ fun ScreenHome(modifier: Modifier = Modifier, mediaItemsViewModel: MediaItemsVie
                 )
             }
             is MediaItemsUIState.Data -> {
-                HorizontalLazyRowWithSnapEffect(uiState.mediaItems)
+                HorizontalLazyRowWithSnapEffect(mediaItemsUIState.mediaItems)
             }
             else -> {
 
--- app/src/main/java/com/niklas/ux62550/MainActivity.kt
+++ app/src/main/java/com/niklas/ux62550/MainActivity.kt
@@ -1,69 +1,32 @@
 package com.niklas.ux62550
 
-import MediaItemsViewModel
+import com.niklas.ux62550.features.MediaItemList.MediaItemsUIState
+import com.niklas.ux62550.features.MediaItemList.MediaItemsViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.List
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Bookmark
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.niklas.ux62550.ui.theme.UX62550Theme
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     private val mediaItemsViewModel: MediaItemsViewModel by viewModels()
@@ -73,11 +36,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UX62550Theme {
+                val uiState = mediaItemsViewModel.mediaItemsState.collectAsState().value
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainScreen(
                         modifier = Modifier.padding(innerPadding),//.background(Color_background)
-                        mediaItemsViewModel = mediaItemsViewModel
+                        mediaItemsUIState = uiState
                     )
                 }
             }
@@ -153,7 +117,7 @@ fun Preview() {
 }*/
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier, mediaItemsViewModel: MediaItemsViewModel) {
+fun MainScreen(modifier: Modifier = Modifier, mediaItemsUIState: MediaItemsUIState) {
 
     UX62550Theme (darkTheme = true, dynamicColor = false) {
         Surface(
@@ -162,7 +126,7 @@ fun MainScreen(modifier: Modifier = Modifier, mediaItemsViewModel: MediaItemsVie
         ) {
 
             ScreenWithGeneralNavBar() {
-                ScreenHome(mediaItemsViewModel = mediaItemsViewModel)
+                ScreenHome(mediaItemsUIState = mediaItemsUIState)
             }
         }
     }
