0e59c1dfe79976fbaf1cd38a3be3f0aa0b8fbd4f
Simon Olsen <s235106@dtu.dk>
2024-11-06 15:52:17 +0100

    I cast badminton smash on Anders - 5 damage

--- app/src/main/java/com/niklas/ux62550/screen/MediaDetailPage.kt
+++ app/src/main/java/com/niklas/ux62550/screen/MediaDetailPage.kt
@@ -1,10 +1,12 @@
 package com.niklas.ux62550.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.MarqueeSpacing
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.aspectRatio
@@ -12,25 +14,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.automirrored.outlined.ArrowLeft
+import androidx.compose.material.icons.automirrored.outlined.StarHalf
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.ArrowLeft
 import androidx.compose.material.icons.outlined.Bookmark
 import androidx.compose.material.icons.outlined.BookmarkBorder
 import androidx.compose.material.icons.outlined.PlayCircleOutline
+import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,7 +71,7 @@ fun MediaDetailPagePreview(){
 
 @Composable
 fun ScreenMediaDetail(modifier: Modifier = Modifier, mediaItemsUIState: MediaItemsUIState) {
-    Column {
+    Column{
         Box(
             modifier.fillMaxWidth()
         ) {
@@ -69,21 +79,23 @@ fun ScreenMediaDetail(modifier: Modifier = Modifier, mediaItemsUIState: MediaIte
                 Box(Modifier.background(Color.Red).fillMaxWidth().height(230.dp))
             }
             Box(
-                modifier.fillMaxWidth().padding(30.dp).absolutePadding(0.dp,40.dp,0.dp,0.dp)
+                modifier.fillMaxWidth().padding(30.dp).absolutePadding(0.dp,40.dp,4.dp,0.dp)
             ) {
                 Box(Modifier.background(Color.Blue).fillMaxWidth().aspectRatio(16f / 9f))
                 Image(
                     Icons.Outlined.PlayCircleOutline,
-                    modifier = Modifier.align(Alignment.Center).padding(12.dp).requiredSize(72.dp),
+                    modifier = Modifier.align(Alignment.Center).absolutePadding(0.dp,0.dp,0.dp,40.dp)
+                        .requiredSize(72.dp),
                     colorFilter = ColorFilter.tint(Color.White),
-                    contentDescription = "Bookmark"
+                    contentDescription = "Play circle"
                 )
+                TitleText()
             }
             Image(
                 Icons.AutoMirrored.Outlined.ArrowBack,
                 modifier = Modifier.padding(12.dp).requiredSize(36.dp),
                 colorFilter = ColorFilter.tint(Color.White),
-                contentDescription = "Bookmark"
+                contentDescription = "Arrow back"
             )
             Image(
                 Icons.Outlined.BookmarkBorder,
@@ -92,8 +104,32 @@ fun ScreenMediaDetail(modifier: Modifier = Modifier, mediaItemsUIState: MediaIte
                 contentDescription = "Bookmark"
             )
 
-
+            Row(
+                modifier = Modifier.align(Alignment.BottomStart).padding(10.dp,0.dp,0.dp,10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                repeat(5) {
+                    Image(
+                        imageVector = Icons.Outlined.StarOutline,
+                        modifier = Modifier.requiredSize(18.dp),
+                        colorFilter = ColorFilter.tint(Color.White),
+                        contentDescription = "Star icon"
+                    )
+                }
+                Text("0/5", fontSize = 18.sp) //Needs get a function for how many stars
+                Text("Year",fontSize = 18.sp)
+                Text("18+", fontSize = 18.sp)
+            }
+            Row(
+                modifier = Modifier.align(Alignment.BottomStart).padding(10.dp,10.dp,0.dp,0.dp),
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text("PLACEHOLDER")
+            }
         }
+        ResumeText()
 
 
         // Implement viewmodel
@@ -115,4 +151,29 @@ fun ScreenMediaDetail(modifier: Modifier = Modifier, mediaItemsUIState: MediaIte
         }
 
     }
+}
+@Composable
+fun TitleText() {
+    Text("RED: The Movie",
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            shadow = Shadow(
+                color = Color.Black, blurRadius = 10f
+            ),
+            textAlign = TextAlign.Center,
+        ),
+        modifier = Modifier.fillMaxWidth().padding(0.dp, 200.dp, 0.dp, 0.dp),
+        )
+}
+@Composable
+fun ResumeText(){
+    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ",
+        style = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+        ),
+        modifier = Modifier.fillMaxWidth().padding(4.dp, 10.dp, 0.dp, 0.dp),
+    )
 }
