cc69c291d37a7402915dd135121c0ae806a46aa2
Anders Greve Sørensen <anders1709@gmail.com>
2024-11-11 09:31:18 +0100

    Move movie and non movie box picture functions into the movie and non movie box files
    Anders improves dramatically at badminton, Simon and Niklas takes 5 fear damage

--- app/src/main/java/com/niklas/ux62550/modules/NonMovieBoxRow.kt
+++ app/src/main/java/com/niklas/ux62550/modules/NonMovieBoxRow.kt
@@ -1,17 +1,35 @@
 package com.niklas.ux62550.modules
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.niklas.ux62550.models.NonMovieBox
-import com.niklas.ux62550.screen.nonMovieBoxMoviePicture
+//import com.niklas.ux62550.screen.nonMovieBoxMoviePicture
+
+@Composable
+fun nonMovieBoxMoviePicture(width: Dp, height: Dp, round: Dp, color: Color, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(color)
+            .size(width, height)
+            .padding()
+    )
+}
 
 @Composable
 fun NonMovieBoxRow(nonMovieBox: NonMovieBox, modifier: Modifier = Modifier) {
