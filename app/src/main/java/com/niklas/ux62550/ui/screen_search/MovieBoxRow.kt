cc0d0508ef6c3ab9bd889b3251e5d0a1adbf5d2a
Anders Greve Soerensen <anders1709@gmail.com>
2024-11-13 10:50:58 +0100

    Add stars to movieBox ratings

--- app/src/main/java/com/niklas/ux62550/ui/screen_search/MovieBoxRow.kt
+++ app/src/main/java/com/niklas/ux62550/ui/screen_search/MovieBoxRow.kt
@@ -1,19 +1,25 @@
 package com.niklas.ux62550.ui.screen_search
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -59,12 +65,25 @@ fun MovieBoxRow(movieBox: MovieBox, modifier: Modifier = Modifier) {
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold
                     )
-                    Text(
-                        text = movieBox.rating.toString() + "/5",
-                        //text =  "3/5",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
+                    Row (modifier
+                        .padding(),
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ){
+                        Image( //Needs to be made button
+                            imageVector = Icons.Filled.Star,
+                            modifier = Modifier.requiredSize(18.dp),
+                            colorFilter = ColorFilter.tint(Color.Yellow),
+                            contentDescription = "Star icon"
+                        )
+                        Text(
+                            text = movieBox.rating.toString() + "/5",
+                            //text =  "3/5",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+
                 }
                 Text(
                     text = " | ",
