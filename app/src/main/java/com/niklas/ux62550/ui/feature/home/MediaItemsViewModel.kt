fdc16020f68c001a004acc48816338e0fc8a58f3
Simon Olsen <s235106@dtu.dk>
2024-11-18 10:17:53 +0100

    Lets start on the view-model Emil

--- app/src/main/java/com/niklas/ux62550/ui/feature/home/MediaItemsViewModel.kt
+++ app/src/main/java/com/niklas/ux62550/ui/feature/home/MediaItemsViewModel.kt
@@ -5,17 +5,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.niklas.ux62550.R
 import com.niklas.ux62550.models.MediaItem
+import com.niklas.ux62550.models.Movie
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.minutes
 
 class MediaItemsViewModel : ViewModel() {
     private val mediaItems: List<MediaItem> = listOf(
         MediaItem("Name 1", R.drawable.logo, Color.Blue),
         MediaItem("Name 2", R.drawable.logo, Color.Red),
         MediaItem("Name 3", R.drawable.logo, Color.Green),
+    )
+    private val movies = listOf(
+        Movie("RED: The Movie", "2022", 131.minutes, 3.5, "N/A", listOf("Action", "Dinosaur Adventure", "Romance"), 18, Color.Red),
+        Movie("Where's the Blue?", "2014", 96.minutes, 2.0, "N/A", listOf("Documentary", "Comedy"), 13, Color.Blue),
+        Movie("Green Man", "1998", 113.minutes, 3.0, "N/A", listOf("Thriller", "Horror"), 15, Color.Green),
     )
 
     private val mutableMediaItemsState = MutableStateFlow<MediaItemsUIState>(MediaItemsUIState.Empty)
