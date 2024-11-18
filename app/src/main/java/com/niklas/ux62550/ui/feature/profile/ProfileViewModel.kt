a472bb3f9843d4bbc516f1d9cad65906b626ad27
Emil Pedersen <steamman0103@gmail.com>
2024-11-18 11:24:19 +0100

    Update to the ProfileViewModel with start to the api calls

--- app/src/main/java/com/niklas/ux62550/ui/feature/profile/ProfileViewModel.kt
+++ app/src/main/java/com/niklas/ux62550/ui/feature/profile/ProfileViewModel.kt
@@ -5,11 +5,22 @@ import androidx.lifecycle.ViewModel
 import com.niklas.ux62550.R
 import com.niklas.ux62550.models.MediaItem
 import com.niklas.ux62550.models.Movie
+import com.niklas.ux62550.models.Profile
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlin.time.Duration.Companion.minutes
 
 class ProfileViewModel : ViewModel() {
+    private  val profile = Profile(
+        name = "Simone",
+        Email = "Simone@gmail.com",
+        FacvritMovieID = 1,
+        backdrop = ,
+        tempColor = Color.Red
+
+    )
+
+
     private val movie = Movie(
         name = "RED: The Movie",
         year = "2022",
