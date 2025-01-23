d66babe7aa438654eda73b52bdca2fc2ae592e18
Christian Livbjerg Worsøe <s235111@dtu.dk>
2025-01-23 16:22:11 +0100

    Fix double blank line >:(

--- app/src/main/java/com/niklas/ux62550/ui/feature/common/MediaItem.kt
+++ app/src/main/java/com/niklas/ux62550/ui/feature/common/MediaItem.kt
@@ -338,7 +338,6 @@ fun MediaItemPreview(round: Dp = 0.dp, modifier: Modifier = Modifier) {
     )
 }
 
-
 // ChatGPT: Animation generated partially and expended on by ourselves
 @Composable
 fun AnimatedImagePlaceholder(modifier: Modifier = Modifier, animationProgress: State<Float>) {
